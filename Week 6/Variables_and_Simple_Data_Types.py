f2b6b8924321ebea496d9eb629f4646d6ede31e5
Zachary Suidan <zsuidan@mail.walshcollege.edu>
2020-10-28 21:52:16 -0400

    Completed variable and data type assignment

---
+++ Week 6/Variables_and_Simple_Data_Types.py
@@ -0,0 +1,37 @@
+#stores first name as a variable
+first_name = "zachary"
+
+#stores last name as variable
+last_name = "SUIDAN"
+
+#Prints out, "Hello, <first name> <last name>" with the first name converted to uppercase letters and the last name converted to lowercase letters using string functions
+print("Hello, " + first_name.upper() + " " + last_name.lower())
+
+#Prints two newlines 
+print("\n\n")
+
+#Prints a quote
+print("\"Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible - Francis of Assisi\"")
+
+#Stores two decimal numbers as variables
+first_num = 2.4
+second_num = 6.3
+
+#Stores operations using the two variables
+num_addition = first_num + second_num
+num_subtraction = first_num - second_num
+num_multiplication = first_num * second_num 
+num_division = second_num / first_num
+
+#Prints the results of the operations
+print(str(first_num) + " plus " + str(second_num) + " equals " + str(num_addition))
+print(str(first_num) + " minus " + str(second_num) + " equals " + str(num_subtraction))
+print(str(first_num) + " times " + str(second_num) + " equals " + str(num_multiplication))
+print(str(second_num) + " divided by " + str(first_num) + " equals " + str(num_division))
+
+#Stores current month as a string and day as a number
+month = "October"
+day = 28
+
+#Prints a statement telling the date
+print("\n\t\tToday is day " + str(day) + " of the month of " + month + ".")
